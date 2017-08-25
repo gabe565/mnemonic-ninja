3cc1c20d6f0f87a44b66a59bab3cfe75603b550b
Gabe Cook <gabe565@users.noreply.github.com>
2017-08-25 00:27:47 -0500

    Remove

--- app/Ipa.php
+++ app/Ipa.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ipa extends Model
 {
-    //
+    public $timestamps = false;
 }
--- app/Letter.php
+++ app/Letter.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Letter extends Model
 {
-    //
+    public $timestamps = false;
 }
