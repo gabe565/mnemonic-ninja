59f3c7cc9e2c0a8f497812443c08af1bde28d0de
Gabe Cook <gabe565@users.noreply.github.com>
2017-09-13 10:47:52 -0500

    New MySQL apt config file

--- .travis.install-mysql-5.7.sh
+++ .travis.install-mysql-5.7.sh
@@ -4,7 +4,7 @@ sudo apt-get autoclean
 sudo rm -rf /var/lib/mysql
 sudo rm -rf /var/log/mysql
 echo mysql-apt-config mysql-apt-config/enable-repo select mysql-5.7 | sudo debconf-set-selections
-wget http://dev.mysql.com/get/mysql-apt-config_0.2.1-1ubuntu12.04_all.deb
-sudo DEBIAN_FRONTEND=noninteractive dpkg -i mysql-apt-config_0.2.1-1ubuntu12.04_all.deb
+wget https://dev.mysql.com/get/mysql-apt-config_0.8.7-1_all.deb
+sudo DEBIAN_FRONTEND=noninteractive dpkg -i mysql-apt-config_0.8.7-1_all.deb
 sudo apt-get update -q
 sudo apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" mysql-server
