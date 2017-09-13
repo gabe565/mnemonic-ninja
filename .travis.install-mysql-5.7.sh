4b6d02d5792b36ee5eb40bf5ae8fafe8dbfc22e7
Gabe Cook <gabe565@users.noreply.github.com>
2017-09-13 14:03:47 -0500

    Oops

--- .travis.install-mysql-5.7.sh
+++ .travis.install-mysql-5.7.sh
@@ -1,5 +1,5 @@
 echo mysql-apt-config mysql-apt-config/enable-repo select mysql-5.7 | sudo debconf-set-selections
-travis_retry wget https://dev.mysql.com/get/mysql-apt-config_0.8.7-1_all.deb
+wget https://dev.mysql.com/get/mysql-apt-config_0.8.7-1_all.deb
 sudo DEBIAN_FRONTEND=noninteractive dpkg -i mysql-apt-config_0.8.7-1_all.deb
 sudo apt-get update -q
 sudo apt-get install -q -y -o Dpkg::Options::=--force-confnew mysql-server
