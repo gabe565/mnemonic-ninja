200b1fe576c6f5757bc02ec320062a1145139781
Gabe Cook <gabe565@users.noreply.github.com>
2017-09-15 00:20:59 -0500

    Fix hr visual glitch

--- resources/views/converters.blade.php
+++ resources/views/converters.blade.php
@@ -7,9 +7,7 @@
         <h1>Converters</h1>
         <hr>
         <converter from="Word(s)" to="Number" url="/api/to/num/" placeholder="example" help="Enter any word to convert" joinstr=", " value="{{ $word }}"></converter>
-        <div class="row">
-            <hr>
-        </div>
+        <hr>
         <converter from="Number" to="Word" url="/api/to/word/" placeholder="70395" help="Enter any number to convert" joinstr=",  " value="{{ $number }}"></converter>
     </div>
 @endsection
