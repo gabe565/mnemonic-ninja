77e8e033ad203439d65ed4cb8b218e5112d340b4
Gabe Cook <gabe565@users.noreply.github.com>
2017-09-15 12:51:09 -0500

    Tweak Travis config

--- .travis.install-mysql-5.7.sh
+++
@@ -1,6 +0,0 @@
-echo mysql-apt-config mysql-apt-config/enable-repo select mysql-5.7 | sudo debconf-set-selections
-wget https://dev.mysql.com/get/mysql-apt-config_0.8.7-1_all.deb
-sudo DEBIAN_FRONTEND=noninteractive dpkg -i mysql-apt-config_0.8.7-1_all.deb
-sudo apt-get update -q
-sudo apt-get install -q -y -o Dpkg::Options::=--force-confnew mysql-server
-sudo mysql_upgrade
