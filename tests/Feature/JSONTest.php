9d931cd667857970763c17388423a5f215de71a2
Gabe Cook <gabe565@users.noreply.github.com>
2017-09-15 23:08:39 -0500

    More API tests

--- tests/Feature/JSONTest.php
+++ tests/Feature/JSONTest.php
@@ -12,13 +12,16 @@ class JSONTest extends TestCase
      *
      * @return void
      */
-    public function testToNum()
+    private function toNum($word, $num)
     {
-        $response = $this->json('GET', '/api/to/num/example');
+        if (!is_array($num)) {
+            $num = [$num];
+        }
+        $response = $this->json('GET', "/api/to/num/$word");
         $response
             ->assertStatus(200)
             ->assertJson([
-                'result' => ['70395'],
+                'result' => $num,
             ]);
     }
 
@@ -27,14 +30,37 @@ class JSONTest extends TestCase
      *
      * @return void
      */
-    public function testToWord()
+    private function toWord($num, $word)
     {
-        $response = $this->json('GET', '/api/to/word/70395');
+        if (!is_array($word)) {
+            $word = [$word];
+        }
+        $response = $this->json('GET', "/api/to/word/$num");
         $response
             ->assertStatus(200)
             ->assertJson([
-                'result' => ['example'],
+                'result' => $word,
             ]);
     }
 
+    public function testAPI() {
+        $this->toNum('example', 70395);
+        $this->toWord(70395, 'example');
+
+        $this->toNum('garage', 746);
+        $this->toNum('garages', 7460);
+        $this->toNum('garagez', 7470);
+
+        $this->toNum('s z', [0, 0]);
+        $this->toNum('t d .th', [1, 1, 1]);
+        $this->toNum('n', 2);
+        $this->toNum('m', 3);
+        $this->toNum('r', 4);
+        $this->toNum('l', 5);
+        $this->toNum('ch j sh', [6, 6, 6]);
+        $this->toNum('ck .c .g k q', [7, 7, 7, 7, 7]);
+        $this->toNum('f .ph v', [8, 8, 8]);
+        $this->toNum('b p', [9, 9]);
+        $this->toNum('.h .y .w a e i o u', []);
+    }
 }
