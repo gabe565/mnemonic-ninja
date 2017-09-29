3fff9442fcb05c88ef10d835a017a9be7d165089
Gabe Cook <gabe565@users.noreply.github.com>
2017-09-29 16:25:24 -0500

    Remove unused Vue js functions and combine help texts into single description

--- resources/assets/js/components/Converter.vue
+++ resources/assets/js/components/Converter.vue
@@ -1,32 +1,37 @@
 <template>
-    <div class="row equal">
-        <div class="col-sm-5 col-md-4 col-md-offset-1 centered" :class="{ 'has-error': error }">
-            <label :for="_uid">{{ from.label }}</label>
-            <textarea :id="_uid" class="form-control conversion" name="query" :placeholder="from.placeholder" autocomplete="off" aria-describedby="help" v-model="query"></textarea>
-            <span class="help-block">{{ from.help }}</span>
-        </div>
-        <div class="col-sm-2 centered">
-            <button type="submit" class="btn btn-success btn-sm">
-                <i class="far fa-2x fa-fw loader" :class="loader" aria-hidden="true"></i>
-            </button>
-        </div>
-        <div class="col-sm-5 col-md-4 centered">
-            <h4>{{ to.label }}</h4>
-            <div class="form-control conversion">
-                <table class="table table-striped">
-                    <tbody>
-                        <tr v-for="(item, key) in result">
-                            <td class="min">{{ key }}:</td>
-                            <td class="result">
-                                <div>
-                                    {{ item }}
-                                </div>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+    <div class="converter">
+        <div class="row">
+            <div class="col-md-10 col-sm-12 col-md-offset-1">
+                <p v-html="description"></p>
             </div>
-            <span class="help-block">{{ to.help }}</span>
+        </div>
+        <div class="row equal">
+            <div class="col-sm-5 col-md-4 col-md-offset-1 centered" :class="{ 'has-error': error }">
+                <label :for="_uid">{{ from.label }}</label>
+                <textarea :id="_uid" class="form-control conversion" name="query" :placeholder="from.placeholder" v-model="query"></textarea>
+            </div>
+            <div class="col-sm-2 centered">
+                <button type="submit" class="btn btn-success btn-sm">
+                    <i class="far fa-2x fa-fw loader" :class="[ loading ? 'fa-sync-alt fa-spin' : 'arrow' ]" aria-hidden="true"></i>
+                </button>
+            </div>
+            <div class="col-sm-5 col-md-4 centered">
+                <h4>{{ to.label }}</h4>
+                <div class="form-control conversion">
+                    <table class="table table-striped">
+                        <tbody>
+                            <tr v-for="(item, key) in result">
+                                <td class="min">{{ key }}:</td>
+                                <td class="result">
+                                    <div>
+                                        {{ item }}
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -51,18 +56,9 @@ export default {
         },
         'url': {
             type: String
-        }
-    },
-    computed: {
-        loader: function() {
-            if (this.loading) {
-                return ['fa-sync-alt', 'fa-spin']
-            } else {
-                return ['arrow']
-            }
         },
-        queries: function() {
-            return this.query.split(' ');
+        'description': {
+            type: String
         }
     },
     watch: {
--- resources/views/converters.blade.php
+++ resources/views/converters.blade.php
@@ -5,6 +5,7 @@
 @section('content')
     <div class="container" id="vue">
         <h1>Converters</h1>
+        <hr>
         <div class="row">
             <div class="col-md-10 col-sm-12 col-md-offset-1">
                 <p>The mnemonic major system aids in numeric memorization by linking numbers with specific phonetic sounds, allowing you to convert a number to a word. This site will help do these conversions for you. <a href="/about">Read more on the about page!</a></p>
@@ -12,8 +13,8 @@
             </div>
         </div>
         <hr>
-        <converter :from="{ label: 'Word', placeholder: 'example', help: 'Enter any word(s) to convert', value: '{{ $word }}' }" :to="{ label: 'Number', help: 'Any corresponding numbers will be shown here.' }" url="/api/to/num/"></converter>
+        <converter :from="{ label: 'Word', placeholder: 'example', value: '{{ $word }}' }" :to="{ label: 'Number' }" description="Enter a word or a list of words to get the number conversions.<br>Note that more than one number may show up from a single word. This means that there is more than one pronounciation!" url="/api/to/num/"></converter>
         <hr>
-        <converter :from="{ label: 'Number', placeholder: 70395, help: 'Enter any number(s) to convert', value: '{{ $number }}' }" :to="{ label: 'Word', help: 'All associated words will be shown here. Note that you may need to scroll through to see all results.' }" url="/api/to/word/"></converter>
+        <converter :from="{ label: 'Number', placeholder: 70395, value: '{{ $number }}' }" :to="{ label: 'Word' }" description="Enter a number or a list of numbers to get all associated words.<br>Note that if many words show up for a single number, you may have to scroll through the results!" url="/api/to/word/"></converter>
     </div>
 @endsection
