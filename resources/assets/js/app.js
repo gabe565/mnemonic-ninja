acc7df93d038317bdbeda267e1cd30c52d1d4146
Gabe Cook <gabe565@users.noreply.github.com>
2017-10-19 16:10:54 -0500

    Fix datatable header location

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -26,7 +26,7 @@ require('datatables.net-responsive');
 
 var table = $('.datatable').DataTable({
     fixedHeader: {
-        headerOffset: 60 //$('.navbar').outerHeight()
+        headerOffset: 70 //$('.navbar').outerHeight()
     },
     info: false,
     ordering: false,
