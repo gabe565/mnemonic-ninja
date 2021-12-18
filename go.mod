418d875349d41b78e69d1549ec3dac5e9fed3cc6
Gabe Cook <gabe565@gmail.com>
2021-12-18 02:11:57 -0600

    :sparkles: Add support for searching by number along with a prettier prompt

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/gabe565/mnemonic-ninja
 go 1.17
 
 require (
+	github.com/AlecAivazis/survey/v2 v2.3.2
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211
 	gorm.io/driver/sqlite v1.2.6
 	gorm.io/gorm v1.22.4
 )
@@ -10,5 +12,11 @@ require (
 require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.3 // indirect
+	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
+	github.com/mattn/go-colorable v0.1.2 // indirect
+	github.com/mattn/go-isatty v0.0.8 // indirect
 	github.com/mattn/go-sqlite3 v1.14.9 // indirect
+	github.com/mgutz/ansi v0.0.0-20170206155736-9520e82c474b // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+	golang.org/x/text v0.3.3 // indirect
 )
--- main.go
+++ main.go
@@ -2,35 +2,56 @@ package main
 
 import (
 	"fmt"
+	"github.com/AlecAivazis/survey/v2"
 	"github.com/gabe565/mnemonic-ninja/internal"
 	"github.com/gabe565/mnemonic-ninja/internal/word"
+	"golang.org/x/term"
+	"io"
+	"os"
 )
 
 func main() {
+	var err error
+
 	db, err := internal.SetupDatabase()
 	if err != nil {
 		panic(err)
 	}
 
-	scanner, err := internal.GetCmudict()
+	body, err := internal.GetCmudict()
+	defer func(resp io.ReadCloser) {
+		_ = resp.Close()
+	}(body)
 	if err != nil {
 		panic(err)
 	}
 
-	err = internal.ImportWords(db, scanner)
+	err = internal.ImportWords(db, body)
 	if err != nil {
 		panic(err)
 	}
 
 	for {
-		fmt.Print("Enter a word to search for: ")
-		var q string
-		_, err := fmt.Scanln(&q)
+		var queryType string
+		err = survey.AskOne(&survey.Select{
+			Message: "Choose lookup method:",
+			Options: []string{"word", "number"},
+		}, &queryType)
 		if err != nil {
-			break
+			return
 		}
+
+		var query string
+		err = survey.AskOne(&survey.Input{
+			Message: "Enter " + queryType + ":",
+		}, &query)
+
 		var words []word.Word
-		if err := db.Where("word = ?", q).Find(&words).Error; err != nil {
+		if err = db.Where(map[string]string{queryType: query}).Find(&words).Error; err != nil {
+			panic(err)
+		}
+
+		if err != nil {
 			fmt.Print(err)
 		}
 		for _, w := range words {
