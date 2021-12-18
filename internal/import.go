596badb33144223dd63843b3ce75262785561d75
Gabe Cook <gabe565@gmail.com>
2021-12-18 02:24:38 -0600

    :loud_sound: Add count logs

--- internal/import.go
+++ internal/import.go
@@ -13,6 +13,7 @@ func ImportWords(db *gorm.DB) error {
 	var err error
 	log.Println("Importing words")
 	s := bufio.NewScanner(strings.NewReader(CMUdict))
+	var lineCount int64
 	err = db.Transaction(func(tx *gorm.DB) error {
 		words := make([]*word.Word, 0, 999)
 		for s.Scan() {
@@ -28,6 +29,7 @@ func ImportWords(db *gorm.DB) error {
 				db.Create(words)
 				words = make([]*word.Word, 0, 999)
 			}
+			lineCount += 1
 		}
 		return nil
 	})
@@ -35,6 +37,11 @@ func ImportWords(db *gorm.DB) error {
 		return err
 	}
 	log.Println("Import complete")
+
+	var count int64
+	db.Model(&word.Word{}).Count(&count)
+	log.Printf("CMUdict line count: %d\n", lineCount)
+	log.Printf("Imported words: %d\n", count)
 
 	_ = body.Close()
 	return nil
