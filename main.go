5b826d13baf32336ed98edf1f195362b2947abf7
Gabe Cook <gabe565@gmail.com>
2021-12-19 21:29:06 -0600

    :recycle: Use string pointers for flags

--- main.go
+++ main.go
@@ -22,17 +22,8 @@ var dist embed.FS
 func main() {
 	var err error
 
-	var address string
-	flag.StringVar(&address, "address", ":3000", "Override listen address.")
-
-	var staticDir string
-	flag.StringVar(
-		&staticDir,
-		"static",
-		"",
-		"Override static asset directory. Useful for development. If left empty, embedded assets are used.",
-	)
-
+	address := flag.String("address", ":3000", "Override listen address.")
+	staticDir := flag.String("static", "", "Override static asset directory. Useful for development. If left empty, embedded assets are used.")
 	flag.Parse()
 
 	db, err := internal.SetupDatabase()
@@ -46,8 +37,8 @@ func main() {
 	}
 
 	var contentFs fs.FS
-	if staticDir != "" {
-		contentFs = os.DirFS(staticDir)
+	if *staticDir != "" {
+		contentFs = os.DirFS(*staticDir)
 	} else {
 		contentFs, err = fs.Sub(dist, "dist")
 		if err != nil {
@@ -55,7 +46,7 @@ func main() {
 		}
 	}
 	router := internal.Router(db, contentFs)
-	err = http.ListenAndServe(address, router)
+	err = http.ListenAndServe(*address, router)
 	if err != nil {
 		panic(err)
 	}
