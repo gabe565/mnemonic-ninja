10758b4a602904cedf04dd35e4c6e20690e3564c
Gabe Cook <gabe565@gmail.com>
2022-01-09 02:08:48 -0600

    :lipstick: About page card layout, other card layout tweaks

--- frontend/src/views/Converters.vue
+++ frontend/src/views/Converters.vue
@@ -3,14 +3,18 @@
     <v-container>
       <v-row>
         <v-col>
-          <p>
-            The mnemonic major system aids in numeric memorization by linking numbers with specific phonetic sounds, allowing you to convert a number to a word.
-            This site will help do these conversions for you.
-            <router-link to="/about">Read more on the about page</router-link>!
-          </p>
-          <p>
-            Fill in a single value or a comma-separated list on the left and view the resulting conversions on the right.
-          </p>
+          <v-card>
+            <v-card-text>
+              <p>
+                The mnemonic major system aids in numeric memorization by linking numbers with specific phonetic sounds, allowing you to convert a number to a word.
+                This site will help do these conversions for you.
+                <router-link to="/about">Read more on the about page</router-link>!
+              </p>
+              <p class="mb-0">
+                Fill in a single value or a comma-separated list on the left and view the resulting conversions on the right.
+              </p>
+            </v-card-text>
+          </v-card>
         </v-col>
       </v-row>
 
@@ -25,39 +29,39 @@
               <v-tab>Word to Number</v-tab>
 
               <v-tab-item>
-                <converter
-                  title="Number to Word"
-                  from-label="Number"
-                  from-placeholder="70395"
-                  :from-value="num"
-                  :from-regex="/[^0-9\s,;]/"
-                  to-label="Word"
-                  url="/api/number"
-                  class="pb-5"
-                >
-                  <h2 class="text-h4 pb-5 text-center">Number to Word</h2>
-                  Enter a number or a list of numbers to get a converted list of words.
-                  <br>
-                  Many words can show up for a single number. If this happens, the result box will be scrollable.
-                </converter>
+                <v-card-text>
+                  <converter
+                    title="Number to Word"
+                    from-label="Number"
+                    from-placeholder="70395"
+                    :from-value="num"
+                    :from-regex="/[^0-9\s,;]/"
+                    to-label="Word"
+                    url="/api/number"
+                  >
+                    Enter a number or a list of numbers to get a converted list of words.
+                    <br>
+                    Many words can show up for a single number. If this happens, the result box will be scrollable.
+                  </converter>
+                </v-card-text>
               </v-tab-item>
 
               <v-tab-item>
-                <converter
-                  title="Word to Number"
-                  from-label="Word"
-                  from-placeholder="example"
-                  :from-value="word"
-                  :from-regex="/[^A-Za-z-'\s,;.]/"
-                  to-label="Number"
-                  url="/api/word"
-                  class="pb-5"
-                >
-                  <h2 class="text-h4 pb-5 text-center">Word to Number</h2>
-                  Enter a word or a list of words to get a converted list of numbers.
-                  <br>
-                  More than one number may show up for a single word. This means there is more than one pronunciation!
-                </converter>
+                <v-card-text>
+                  <converter
+                    title="Word to Number"
+                    from-label="Word"
+                    from-placeholder="example"
+                    :from-value="word"
+                    :from-regex="/[^A-Za-z-'\s,;.]/"
+                    to-label="Number"
+                    url="/api/word"
+                  >
+                    Enter a word or a list of words to get a converted list of numbers.
+                    <br>
+                    More than one number may show up for a single word. This means there is more than one pronunciation!
+                  </converter>
+                </v-card-text>
               </v-tab-item>
             </v-tabs>
           </v-card>
