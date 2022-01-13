3f0b4efc22bd5d6cad49a78e2830259427a43882
Gabe Cook <gabe565@gmail.com>
2022-01-12 19:09:14 -0600

    :white_check_mark: Add QueryType tests

---
+++ internal/server/querytype_test.go
@@ -0,0 +1,48 @@
+package server
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestWhereColumn(t *testing.T) {
+	testCases := []struct {
+		queryType QueryType
+		expect    string
+	}{
+		{Word, "word"},
+		{Number, "number"},
+		{99, "QueryType(99)"},
+	}
+	for _, tc := range testCases {
+		tc := tc // capture range variable
+		t.Run(fmt.Sprintf("Get where column for QueryType %v", tc.queryType), func(t *testing.T) {
+			t.Parallel()
+			got := tc.queryType.WhereColumn()
+			if got != tc.expect {
+				t.Errorf("invalid where column. got %s, want %s", got, tc.expect)
+			}
+		})
+	}
+}
+
+func TestDistinctColumn(t *testing.T) {
+	testCases := []struct {
+		queryType QueryType
+		expect    string
+	}{
+		{Word, "word"},
+		{Number, "number"},
+		{99, "QueryType(99)"},
+	}
+	for _, tc := range testCases {
+		tc := tc // capture range variable
+		t.Run(fmt.Sprintf("Get where column for QueryType %v", tc.queryType), func(t *testing.T) {
+			t.Parallel()
+			got := tc.queryType.WhereColumn()
+			if got != tc.expect {
+				t.Errorf("invalid where column. got %s, want %s", got, tc.expect)
+			}
+		})
+	}
+}
