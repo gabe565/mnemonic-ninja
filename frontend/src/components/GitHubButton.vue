4ae12ac37e784f3c48ed11fa73a39e65154610f5
Gabe Cook <gabe565@gmail.com>
2022-01-21 15:13:06 -0600

    :lock: Add noopener

--- frontend/src/components/GitHubButton.vue
+++ frontend/src/components/GitHubButton.vue
@@ -3,7 +3,8 @@
     <template #activator="{ on, attrs }">
       <v-btn
         icon
-        href="https://github.com/gabe565/mnemonic-ninja" target="_blank"
+        href="https://github.com/gabe565/mnemonic-ninja"
+        target="_blank" rel="noopener"
         v-bind="attrs" v-on="on">
         <v-icon>fab fa-github</v-icon>
       </v-btn>
