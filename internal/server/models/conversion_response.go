97a80dab822057edc22f388d74c8d38f8443593b
Gabe Cook <gabe565@gmail.com>
2022-03-21 00:22:39 -0500

    :fire: Simplify conversion response render logic

--- internal/server/models/conversion_response.go
+++ internal/server/models/conversion_response.go
@@ -14,17 +14,12 @@ type ConversionResponse struct {
 func (response *ConversionResponse) Render(w http.ResponseWriter, r *http.Request) (err error) {
 	for _, result := range response.Result {
 		for _, w := range result.Words {
-			var responseEntry any
 			switch response.QueryType {
 			case Word:
-				responseEntry = w.Number
+				result.Result = append(result.Result, w.Number)
 			case Number:
-				responseEntry = w.Word
+				result.Result = append(result.Result, w.Word)
 			}
-			if err != nil {
-				return err
-			}
-			result.Result = append(result.Result, responseEntry.(string))
 			if w.Guess {
 				result.Guess = w.Guess
 			}
