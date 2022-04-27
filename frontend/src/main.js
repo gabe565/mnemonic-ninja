c044b121619df7336c6041211d738d1419953d43
Gabe Cook <gabe565@gmail.com>
2022-04-27 16:03:41 -0500

    :rotating_light: Fix lint import order error

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue';
-import App from './App.vue';
 import '@/registerServiceWorker';
 import router from '@/router';
 import vuetify from '@/plugins/vuetify';
+import App from './App.vue';
 import '@fortawesome/fontawesome-pro/css/fontawesome.css';
 import '@fortawesome/fontawesome-pro/css/solid.css';
 import '@fortawesome/fontawesome-pro/css/brands.css';
