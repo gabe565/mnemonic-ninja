1246c08face16f816091445c7bc15677a9d23908
Gabe Cook <gabe565@gmail.com>
2022-05-04 16:04:19 -0500

    :sparkles: Add config file support

--- main.go
+++ main.go
@@ -5,40 +5,32 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"github.com/gabe565/mnemonic-ninja/internal/config"
 	"github.com/gabe565/mnemonic-ninja/internal/database"
 	"github.com/gabe565/mnemonic-ninja/internal/database/seeds"
 	"github.com/gabe565/mnemonic-ninja/internal/server"
 	flag "github.com/spf13/pflag"
+	"github.com/spf13/viper"
 	"io/fs"
 	"log"
 	"net/http"
 	"os"
-	"strings"
 )
-
-const EnvPrefix = "MNEMONIC_NINJA_"
 
 //go:embed .cmudict.dict.gz
 var cmudictGz []byte
 
+func init() {
+	flag.String("address", ":3000", "Override listen address.")
+	flag.String("frontend", defaultFrontend, "Override frontend asset directory."+frontendHelpExt)
+}
+
 func main() {
 	var err error
 
-	address := flag.String("address", ":3000", "Override listen address.")
-	frontendDir := flag.String("frontend", defaultFrontend, "Override frontend asset directory."+frontendHelpExt)
-	flag.Parse()
-
-	flag.CommandLine.VisitAll(func(f *flag.Flag) {
-		optName := strings.ToUpper(f.Name)
-		optName = strings.ReplaceAll(optName, "-", "_")
-		varName := EnvPrefix + optName
-		if val, ok := os.LookupEnv(varName); !f.Changed && ok {
-			err = f.Value.Set(val)
-			if err != nil {
-				log.Fatalln(err)
-			}
-		}
-	})
+	if err = config.Parse(); err != nil {
+		panic(err)
+	}
 
 	db, err := database.Setup()
 	if err != nil {
@@ -51,8 +43,9 @@ func main() {
 	}
 
 	var frontendFs fs.FS
-	if *frontendDir != "" {
-		frontendFs = os.DirFS(*frontendDir)
+	frontendDir := viper.GetString("frontend")
+	if frontendDir != "" {
+		frontendFs = os.DirFS(frontendDir)
 	} else {
 		frontendFs, err = fs.Sub(frontendEmbed, "frontend/dist")
 		if err != nil {
@@ -60,8 +53,9 @@ func main() {
 		}
 	}
 	router := server.Router(db, frontendFs)
-	log.Println("Listening on " + *address)
-	err = http.ListenAndServe(*address, router)
+	address := viper.GetString("address")
+	log.Println("Listening on " + address)
+	err = http.ListenAndServe(address, router)
 	if err != nil {
 		panic(err)
 	}
