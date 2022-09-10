611cc95938389d0622b5dac050f56fdd173e37da
Gabe Cook <gabe565@gmail.com>
2022-09-10 00:20:58 -0500

    :rotating_light: More lint fixes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,21 +1,19 @@
-import { VuetifyResolver } from 'unplugin-vue-components/resolvers';
-import { createVuePlugin as vue } from 'vite-plugin-vue2';
-import Components from 'unplugin-vue-components/vite';
-import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa';
+import { VuetifyResolver } from "unplugin-vue-components/resolvers";
+import { createVuePlugin as vue } from "vite-plugin-vue2";
+import Components from "unplugin-vue-components/vite";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
     vue(),
     Components({
       directives: false,
-      resolvers: [
-        VuetifyResolver(),
-      ],
+      resolvers: [VuetifyResolver()],
       types: [
         {
-          from: 'vue-router',
-          names: ['RouterLink', 'RouterView'],
+          from: "vue-router",
+          names: ["RouterLink", "RouterView"],
         },
       ],
     }),
@@ -24,45 +22,42 @@ export default defineConfig({
       devOptions: {
         enabled: true,
       },
-      includeAssets: [
-        'favicon.ico',
-        'img/icons/*',
-      ],
+      includeAssets: ["favicon.ico", "img/icons/*"],
       manifest: {
-        name: 'Mnemonic Ninja',
-        short_name: 'Mnemonic Ninja',
-        id: '/',
-        description: 'Convert a number into a phrase to aid in memorization.',
-        theme_color: '#607D8B',
-        background_color: '#607D8B',
+        name: "Mnemonic Ninja",
+        short_name: "Mnemonic Ninja",
+        id: "/",
+        description: "Convert a number into a phrase to aid in memorization.",
+        theme_color: "#607D8B",
+        background_color: "#607D8B",
         icons: [
           {
-            src: 'img/icons/android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
+            src: "img/icons/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
           },
           {
-            src: 'img/icons/android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
+            src: "img/icons/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
           },
           {
-            src: 'img/icons/android-chrome-maskable-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-            purpose: 'maskable',
+            src: "img/icons/android-chrome-maskable-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
           },
           {
-            src: 'img/icons/android-chrome-maskable-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'maskable',
+            src: "img/icons/android-chrome-maskable-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
           },
         ],
       },
       workbox: {
         clientsClaim: true,
-        globPatterns: ['**/*{js,css,html,woff2}'],
+        globPatterns: ["**/*{js,css,html,woff2}"],
       },
     }),
   ],
