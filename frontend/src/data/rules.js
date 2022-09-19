82ead3e471fc8c6f9775015a1eaa11389cb4dab5
Gabe Cook <gabe565@gmail.com>
2022-09-19 01:55:15 -0500

    :bug: Fix conversion rule table contents

--- frontend/src/data/rules.js
+++ frontend/src/data/rules.js
@@ -18,106 +18,106 @@ export default {
     new Rule(
       "0",
       ["/s/,", "/z/"],
-      ["__s__,", "soft __c__,", "__z__,", "__x__ (in __xylophone__)"],
+      ["<i>s</i>,", "soft <i>c</i>,", "<i>z</i>,", "<i>x</i> (in <i>xylophone</i>)"],
       [
-        "__Zero__ begins with __z__.",
-        "Upper case __S__ and __Z__, as well as lower case __s__ and __z__, have __zero__ vertical strokes each, as with the number __0__.",
-        "The alveolar fricatives /s/ and /z/ form a [voiceless and voiced pair](https://en.wikipedia.org/wiki/Voice_(phonetics)).",
+        "<i>Zero</i> begins with <i>z</i>.",
+        "Upper case <i>S</i> and <i>Z</i>, as well as lower case <i>s</i> and <i>z</i>, have <i>zero</i> vertical strokes each, as with the number <i>0</i>.",
+        'The alveolar fricatives /s/ and /z/ form a <a href="https://en.wikipedia.org/wiki/Voice_(phonetics)" target="_blank">voiceless and voiced pair</a>.',
       ]
     ),
     new Rule(
       "1",
       ["/t/,", "/d/,", "(/θ/, /ð/)"],
-      ["__t__,", "__d__,", "(__th__ in __thing__ and __this__)"],
+      ["<i>t</i>,", "<i>d</i>,", "(<i>th</i> in <i>thing</i> and <i>this</i>)"],
       [
-        "Upper case __T__ and __D__, as well as lower case __t__ and __d__ have one vertical stroke each, as with the number __1__.",
+        "Upper case <i>T</i> and <i>D</i>, as well as lower case <i>t</i> and <i>d</i> have one vertical stroke each, as with the number <i>1</i>.",
         "The alveolar stops /t/ and /d/ form a voiceless and voiced pair, as do the similar-sounding dental fricatives /θ/ and /ð/, though some variant systems may omit the latter pair.",
       ]
     ),
     new Rule(
       "2",
       ["/n/"],
-      ["__n__"],
+      ["<i>n</i>"],
       [
-        "Upper case __N__ and lower case __n__ each have __two__ vertical strokes and __two__ points on the [baseline](https://en.wikipedia.org/wiki/Baseline_(typography)).",
+        'Upper case <i>N</i> and lower case <i>n</i> each have <i>two</i> vertical strokes and <i>two</i> points on the <a href="https://en.wikipedia.org/wiki/Baseline_(typography)" target="_blank">baseline</a>.',
       ]
     ),
     new Rule(
       "3",
       ["/m/"],
-      ["__m__"],
+      ["<i>m</i>"],
       [
-        "Lower case __m__ has __three__ vertical strokes.",
-        "Both upper case __M__ and lower case __m__ each have __three__ points on the baseline and look like the number __3__ on its side.",
+        "Lower case <i>m</i> has <i>three</i> vertical strokes.",
+        "Both upper case <i>M</i> and lower case <i>m</i> each have <i>three</i> points on the baseline and look like the number <i>3</i> on its side.",
       ]
     ),
     new Rule(
       "4",
       ["/r/"],
-      ["__r__,", "__l__ (in __colonel__)"],
-      ["__Four__ ends with __r__ (and /r/ in rhotic accents)."]
+      ["<i>r</i>,", "<i>l</i> (in <i>colonel</i>)"],
+      ["<i>Four</i> ends with <i>r</i> (and /r/ in rhotic accents)."]
     ),
     new Rule(
       "5",
       ["/l/"],
-      ["__l__"],
+      ["<i>l</i>"],
       [
-        "__L__ is the [Roman number](https://en.wikipedia.org/wiki/Roman_number) for __5__0.",
-        "Among the __five__ digits of one's __l__eft hand, the thumb and index fingers also form an __L__",
+        '<i>L</i> is the <a href="https://en.wikipedia.org/wiki/Roman_number" target="_blank">Roman number</a> for <i>5</i>0.',
+        "Among the <i>five</i> digits of one's <i>l</i>eft hand, the thumb and index fingers also form an <i>L</i>",
       ]
     ),
     new Rule(
       "6",
       ["/tʃ/,", "/dʒ/,", "/ʃ/,", "/ʒ/"],
       [
-        "__ch__ (in __cheese__ and __chef__),",
-        "__j__,",
-        "soft __g__,",
-        "__sh__,",
-        "__c__ (in __cello__ and __special__),",
-        "__cz__ (in __Czech__),",
-        "__s__ (in __tissue__ and __vision__),",
-        "__sc__ (in __fascist__),",
-        "__sch__ (in __schwa__ and __eschew__),",
-        "__t__ (in __ration__ and __equation__),",
-        "__tsch__ (in __putsch__),",
-        "__z__ (in __seizure__)",
+        "<i>ch</i> (in <i>cheese</i> and <i>chef</i>),",
+        "<i>j</i>,",
+        "soft <i>g</i>,",
+        "<i>sh</i>,",
+        "<i>c</i> (in <i>cello</i> and <i>special</i>),",
+        "<i>cz</i> (in <i>Czech</i>),",
+        "<i>s</i> (in <i>tissue</i> and <i>vision</i>),",
+        "<i>sc</i> (in <i>fascist</i>),",
+        "<i>sch</i> (in <i>schwa</i> and <i>eschew</i>),",
+        "<i>t</i> (in <i>ration</i> and <i>equation</i>),",
+        "<i>tsch</i> (in <i>putsch</i>),",
+        "<i>z</i> (in <i>seizure</i>)",
       ],
       [
-        "Upper case __G__ and lower case __g__ look like the number __6__ flipped horizontally and rotated 180° respectively.",
-        'Lower case <a href="https://en.wikipedia.org/wiki/Script_typeface" target="_blank">script</a> __j__ tends to have a lower loop, like the number __6__.',
-        'In some <a href="https://en.wikipedia.org/wiki/Serif" target="_blank">serif</a> fonts, upper case __CH__, __SH__ and __ZH__ each have __six__ serifs.',
+        "Upper case <i>G</i> and lower case <i>g</i> look like the number <i>6</i> flipped horizontally and rotated 180° respectively.",
+        'Lower case <a href="https://en.wikipedia.org/wiki/Script_typeface" target="_blank">script</a> <i>j</i> tends to have a lower loop, like the number <i>6</i>.',
+        'In some <a href="https://en.wikipedia.org/wiki/Serif" target="_blank">serif</a> fonts, upper case <i>CH</i>, <i>SH</i> and <i>ZH</i> each have <i>six</i> serifs.',
         "The postalveolar affricates /tʃ/ and /dʒ/ form a voiceless and voiced pair, as do the similar-sounding postalveolar fricatives /ʃ/ and /ʒ/.",
-        "__CH__urch has six letters.",
+        "<i>CH</i>urch has six letters.",
       ]
     ),
     new Rule(
       "7",
       ["/k/,", "/ɡ/"],
-      ["__k__,", "hard __c__,", "__q__,", "__ch__ (in loch),", "hard __g__"],
+      ["<i>k</i>,", "hard <i>c</i>,", "<i>q</i>,", "<i>ch</i> (in loch),", "hard <i>g</i>"],
       [
-        "Both upper case __K__ and lower case __k__ look like two small __7__s on their sides.",
-        "In some fonts, the lower-right part of the upper case __G__ looks like a __7__.",
-        "__G__ is also the __7__th letter of the alphabet.",
+        "Both upper case <i>K</i> and lower case <i>k</i> look like two small <i>7</i>s on their sides.",
+        "In some fonts, the lower-right part of the upper case <i>G</i> looks like a <i>7</i>.",
+        "<i>G</i> is also the <i>7</i>th letter of the alphabet.",
         "The velar stops /k/ and /g/ form a voiceless and voiced pair.",
       ]
     ),
     new Rule(
       "8",
       ["/f/,", "/v/"],
-      ["__f__,", "__ph__ (in __phone__),", "__v__,", "__gh__ (in __laugh__)"],
+      ["<i>f</i>,", "<i>ph</i> (in <i>phone</i>),", "<i>v</i>,", "<i>gh</i> (in <i>laugh</i>)"],
       [
-        "Lower case script __f__, which tends to have an upper and lower loop, looks like a __f__igure-__8__.",
+        "Lower case script <i>f</i>, which tends to have an upper and lower loop, looks like a <i>f</i>igure-<i>8</i>.",
         "The labiodental fricatives /f/ and /v/ form a voiceless and voiced pair.",
       ]
     ),
     new Rule(
       "9",
       ["/p/,", "/b/"],
-      ["__p__,", "__b__,", "__gh__ (in __hiccough__)"],
+      ["<i>p</i>,", "<i>b</i>,", "<i>gh</i> (in <i>hiccough</i>)"],
       [
-        "Upper case __P__ and lower case __p__ look like the number __9__ flipped horizontally.",
-        "Lower case __b__ looks like the number __9__ turned 180°.",
+        "Upper case <i>P</i> and lower case <i>p</i> look like the number <i>9</i> flipped horizontally.",
+        "Lower case <i>b</i> looks like the number <i>9</i> turned 180°.",
         "The labial stops /p/ and /b/ form a voiceless and voiced pair.",
       ]
     ),
@@ -130,14 +130,14 @@ export default {
         '<a href="https://en.wikipedia.org/wiki/English_phonology#Vowels" target="_blank">vowel sounds</a>',
       ],
       [
-        "__h__,__y__,__w__,__a__,__e__,__i__,__o__,__u__,",
+        "<i>h</i>,<i>y</i>,<i>w</i>,<i>a</i>,<i>e</i>,<i>i</i>,<i>o</i>,<i>u</i>,",
         '<a href="https://en.wikipedia.org/wiki/Silent_letter" target="_blank">silent letters</a>,',
-        "__c__ (in __packet__ and __chutzpah__),",
-        "__d__ (in __judge__),",
-        "__j__ (in __Hallelujah__ and __jalapeno__),",
-        "__ll__ (in __tortilla__),",
-        "the first __p__ in __sapphire__,",
-        "__t__ (in __match__),",
+        "<i>c</i> (in <i>packet</i> and <i>chutzpah</i>),",
+        "<i>d</i> (in <i>judge</i>),",
+        "<i>j</i> (in <i>Hallelujah</i> and <i>jalapeno</i>),",
+        "<i>ll</i> (in <i>tortilla</i>),",
+        "the first <i>p</i> in <i>sapphire</i>,",
+        "<i>t</i> (in <i>match</i>),",
         "one of doubled letters in most contexts",
       ],
       [
@@ -147,9 +147,15 @@ export default {
     new Rule(
       "(2, 27 or 7)",
       ["/ŋ/"],
-      ["__ng__,", "__n__ before __k__,", "hard __c__,", "__q__,", "hard __g__ or __x__"],
       [
-        "Variant systems differ about whether /ŋ/ should encode __2__ and classified together with /n/, __7__ and classified together with /k/ and /g/ or even __27__ (e.g. __ring__ could be __42__, __47__ or __427__). When a /k/ and /g/ is pronounced separately after the /ŋ/, variant systems that chose /ŋ/ to be __27__ also disagree if an extra __7__ should be written (e.g. finger could be __8274__ or __82774__, or if /ŋ/ is chosen to be __7__, __8774__).",
+        "<i>ng</i>,",
+        "<i>n</i> before <i>k</i>,",
+        "hard <i>c</i>,",
+        "<i>q</i>,",
+        "hard <i>g</i> or <i>x</i>",
+      ],
+      [
+        "Variant systems differ about whether /ŋ/ should encode <i>2</i> and classified together with /n/, <i>7</i> and classified together with /k/ and /g/ or even <i>27</i> (e.g. <i>ring</i> could be <i>42</i>, <i>47</i> or <i>427</i>). When a /k/ and /g/ is pronounced separately after the /ŋ/, variant systems that chose /ŋ/ to be <i>27</i> also disagree if an extra <i>7</i> should be written (e.g. finger could be <i>8274</i> or <i>82774</i>, or if /ŋ/ is chosen to be <i>7</i>, <i>8774</i>).",
       ]
     ),
   ],
