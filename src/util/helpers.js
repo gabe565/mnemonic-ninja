05fc30187ce79e245d6a0fc0dd78633949ce7933
Gabe Cook <gabe565@gmail.com>
2023-01-31 00:32:10 -0600

    :fire: Remove unused wait helper

--- src/util/helpers.js
+++ src/util/helpers.js
@@ -1,6 +1,1 @@
-export const wait = (timeout) =>
-  new Promise((resolve) => {
-    setTimeout(resolve, timeout);
-  });
-
 export const castArray = (...args) => (args[0] instanceof Array ? args[0] : args);
