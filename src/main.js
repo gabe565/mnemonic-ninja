26433a06f8df905cbc7587f9ab2611c8d13ec954
Gabe Cook <gabe565@gmail.com>
2023-05-22 15:52:38 -0500

    :truck: Move router config to plugins dir

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "./router";
+import router from "./plugins/router";
 import vuetify from "./plugins/vuetify";
 
 createApp(App).use(router).use(vuetify).mount("#app");
