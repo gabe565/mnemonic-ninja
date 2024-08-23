0029db1b838ff40fef0761519e55caf5c8397f91
Gabe Cook <gabe565@gmail.com>
2024-08-23 03:56:57 -0500

    feat(plausible): Add Plausible

---
+++ src/plugins/plausible.js
@@ -0,0 +1,10 @@
+import Plausible from "plausible-tracker";
+
+export const plausible = Plausible({
+  apiHost: import.meta.env.VITE_PLAUSIBLE_HOST,
+});
+
+if (import.meta.env.VITE_PLAUSIBLE_HOST) {
+  plausible.enableAutoPageviews();
+  plausible.enableAutoOutboundTracking();
+}
